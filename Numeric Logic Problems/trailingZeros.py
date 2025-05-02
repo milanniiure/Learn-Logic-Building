70c1382864260de6fb7563bb810188998188c0dd
me.learn <provinesmilan@gmail.com>
2025-05-02 22:17:43 +0545

    Concept updated for better understanding

--- Numeric Logic Problems/trailingZeros.py
+++ Numeric Logic Problems/trailingZeros.py
@@ -6,3 +6,13 @@
 # n=5 and 5! (5*4*3*2*1) has one 5 and three 2s (5*2*2*3*2*1). So the count of trailing 0s is min(1,3) = 1
 # n=11 and 11! (11*10*9*8*7*6*5*4*3*2*1) has (11*5*2*3*3*2*2*2*7*2*3*5*2*2*3*2*1) has two 5s and eight 2s. 
 # so the count of trailing 0 is min(8, 2) = 2
+
+# we can observe that the number of 2s in prime factors is always more than or equal to the number of 5s. so we are
+# counting 5s and we are done.
+
+
+#      SO HOW TO CALCULATE NOW 
+# Trailing 0s in n! = Count of 5s in prime factors of n! = floor(n/5) + floor(n/25) + floor(n/125)+ .. 
+# until n//5^k is 0.
+
+
