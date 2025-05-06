171f4e98fa4a8fd5fc6329541415db51f2732803
me.learn <provinesmilan@gmail.com>
2025-05-06 22:53:33 +0545

    Concept: Modular Exponentiation

---
+++ Numeric Logic Problems/modularExponentiation.py
@@ -0,0 +1,8 @@
+# solve the modular Exponentiation efficiently ( Used in cryptography, competitive programming, modular arthmetic
+
+# what is modular exponentiation? 
+# Answer : (a power b modulus c) 3^13 % 7 
+# (3^13%7 = 1594323 % 7, its inefficient)
+
+# Efficient way to solve it : Exponentiation by Squaring with Modulo 
+# 
