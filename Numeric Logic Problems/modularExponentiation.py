37cf079eba702105019a51964bce711be7c2adef
me.learn <provinesmilan@gmail.com>
2025-05-08 14:17:54 +0545

    Problem : Calculate modular exponentiation efficiently

--- Numeric Logic Problems/modularExponentiation.py
+++ Numeric Logic Problems/modularExponentiation.py
@@ -1,31 +1,20 @@
-# solve the modular Exponentiation efficiently ( Used in cryptography, competitive programming, modular arthmetic
+# Calculate the modular exponentiation and see how it works
 
-# Modular Exponentiation : Naive approach:
-# Answer : (a power b modulus c) 3^13 % 7 
-# (3^13%7 = 1594323 % 7, its inefficient)
+def modularExp(base, exponent, modulus):
+    result = 1
+    base = base % modulus
 
-# Efficient Method: Square and Multiply Method (Binary Exponentiation)
-# convert expoenent to binary (to check bits)
-# initialize result with 1
-# square the result, do mod and store it on result
-# multiply the result by base (if bit is 1) otherwise skip multiplication part , do mod and store it on result var
-# after processing all bits final answer will be result var
+    while exponent > 0:
+        if exponent % 2 == 1:
+            result = (result * base)% modulus
+        base = (base*base) % modulus
+        exponent = exponent // 2
+    return result
 
-# Example:
-# 7^5 % 9   (a power b mod c)
-# result has 1 as starting value
-# to know bits : 5 = 101 (convert exponent into binary)
+# user input
+baseNumber = int(input("Enter the base number: "))
+exponentNumber = int(input("Enter the exponent number: "))
+modulusNumber = int(input("Enter the modulus number: "))
+answer = modularExp(baseNumber, exponentNumber, modulusNumber)
 
-# SQUARE: 1**2 % 9 = 1         (result X power mod c )
-# MULTIPLY : 1*7 % 9 = 7       (result X base mod c)
-
-# now the result has 7
-# SQUARE: 7**2 % 9 = 4         (result x power mod c)
-# MULTIPLY: skip, current bit is 0 
-
-# now the result has 4
-# SQUARE: 4**2 % 9 = 7         (result x power mod c)
-# MULTIPLY: 7*7 % 9 = 4        (result X base mod c)
-
-# 101 (three bits completed) result stores 4 
-# ANSWER IS 4
+print(f"Modular exponentiation of ({baseNumber}**{exponentNumber}) % {modulusNumber} is {answer}")
